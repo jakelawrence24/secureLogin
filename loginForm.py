a2e7ee1c213cde3e06217807de1bbb37d457b3ad
Jake Lawrence <jake.lawrence24@gmail.com>
2016-11-23 16:39:52 -0500

    encrypted the database

--- loginForm.py
+++ loginForm.py
@@ -7,9 +7,11 @@
 
 # used sqlite3 so I wouldn't need to write a full stack
 import sqlite3
+import hashlib
+from hashlib import md5
 
 
-# the database is:
+# the unencrypted database is:
 # ** username *   password  **
 # ****************************
 # 1* admin    * admin123    **
@@ -23,7 +25,7 @@ import sqlite3
 # ****************************
 
 # references to the database attached
-database = 'login_info.sqlite'
+database = 'encrypted_login_info.sqlite'
 nameOfTable = 'login_table'
 usernameCol = 'username'
 passwordCol = 'password'
@@ -38,24 +40,33 @@ print("Login Below\n")
 username = raw_input("Username: ")
 password = raw_input("Password: ")
 
+# converting plain text to md5 hash
+m = hashlib.md5()
+
+m.update(username.encode('utf-8'))
+md5_username = m.hexdigest()
+
+m.update(password.encode('utf-8'))
+md5_password = m.hexdigest()
+
 # determines if the username is in the database
 c.execute('SELECT {c1} FROM {tn} WHERE {cn}=?'.\
-        format(c1=usernameCol, tn=nameOfTable, cn=usernameCol),(username,))
+        format(c1=usernameCol, tn=nameOfTable, cn=usernameCol),(md5_username,))
 usernameExists = c.fetchone()
 
 # if the username is in the database
 if usernameExists:
 	# gets the row index for the username
 	c.execute('SELECT rowid, * FROM {tn} WHERE {cn} = ?'.\
-		format(tn=nameOfTable, cn=usernameCol),(username,))
+		format(tn=nameOfTable, cn=usernameCol),(md5_username,))
 	username_row = c.fetchone()[0]
 	# determines if the password exists
 	c.execute('SELECT {c2} FROM {tn} WHERE {cn}=?'.\
-        format(c2=usernameCol, tn=nameOfTable, cn=passwordCol), (password,))
+        format(c2=usernameCol, tn=nameOfTable, cn=passwordCol), (md5_password,))
 	passwordExists= c.fetchone()
 	# gets the row index of the password
 	c.execute('SELECT rowid, * FROM {tn} WHERE {cn} = ?'.\
-		format(tn=nameOfTable, cn=passwordCol),(password,))
+		format(tn=nameOfTable, cn=passwordCol),(md5_password,))
 	try:
 		# sets the row of the password to a variable
 		password_row = c.fetchone()[0]
